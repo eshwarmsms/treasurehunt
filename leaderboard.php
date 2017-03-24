5b52d045bdf2b45f998415d5c9fde4f9e4c41060
eshwar <eshwarmsms@gmail.com>
2017-03-25 00:08:55 +0530

    bug fix

--- leaderboard.php
+++ leaderboard.php
@@ -7,7 +7,7 @@
 <body>
 
     <div id="frame">
-       Welcome <strong><?php echo($user) ?></strong>
+        <strong><?php echo($user) ?></strong>
         <a href="/hunt.php">PUZZLES</a>
         <a href="/logout.php">LOGOUT</a>
         <h1>LEADERBOARD</h1>
