bc91a165bd1a9ec70d2a4e7f00276a101c05fa5b
eshwar <eshwarmsms@gmail.com>
2017-03-24 11:32:42 +0530

    show current question of user

--- hunt.php
+++ hunt.php
@@ -40,13 +40,34 @@ else{
     header("location:index.php"); // redirects if user is not logged in
 }
 $user = $_SESSION['user']; //assigns user value
+    
+mysql_connect("in-cdbr-azure-south-c.cloudapp.net", "bf142daa6c0a1f","97095cdc", "treasurehunt") or die(mysql_error()); //Connect to server
+mysql_select_db("treasurehunt") or die("Cannot connect to database"); //Connect to database
+$query = mysql_query("SELECT * from answer, user WHERE answer.email='$user' and users.current = answer.question"); //Query the users table if there are matching rows equal to $username
+$exists = mysql_num_rows($query); //Checks if username exists
+if($exists > 0) //IF there are no returning rows or no existing username
+{
+    while($row = mysql_fetch_assoc($query)) //display all rows from query
+    {
+        $table_question = $row['question']; // the first username row is passed on to $table_users, and so on until the query is finished
+        // $table_answer = $row['answer']; // the first password row is passed on to $table_users, and so on until the query is finished
+        $table_hint = $row['hint'];
+    }
+    
+}
+else
+{
+    Print '<script>alert("Somethings Wrong, contact admin");</script>'; //Prompts the user
+    //Print '<script>window.location.assign("index.php");</script>'; // redirects to login.php
+}
 ?>
 
 
 <div style="margin-right: auto;margin-left: auto;text-align:center;max-width:800px;padding:80px;box-shadow: 0 8px 6px -6px black;";>
 
 <form method="post" action="form.php" name="ContactForm" onsubmit="return ValidateContactForm();">
-    <img src="kalam.jpg" alt="image"></img><br>
+    <img src="<?php echo("images/" +$table_question+ ".png") ?>" alt="image"></img><br>
+    <p>Hint: first letter is <?php echo($table_hint) ?> </p>
     <p>Answer: <input type="text" size="65" name="Name"></p>
     <p><input type="submit" value="Send" name="submit">
     <input type="reset" value="Reset" name="reset"></p>
