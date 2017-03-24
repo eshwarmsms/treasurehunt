a446a57597bf4d226cb3172896052c01c39e20ea
eshwar <eshwarmsms@gmail.com>
2017-03-24 15:02:58 +0530

    add leaderboard

---
+++ leaderboard.php
@@ -0,0 +1,109 @@
+<html>
+<head>    
+    <title>Treasure Hunt</title>
+    <style>
+    
+        @media (width: 500px){
+            form{
+                font-size: 15px; 
+            }
+            #frame{
+			    padding:20px;
+            }
+        }
+        input[type=submit] , [type=reset]{
+			background-color: #4CAF50;
+			border: none;
+			color: white;
+			padding: 16px 32px;
+			text-decoration: none;
+			margin: 4px 2px;
+			cursor: pointer;
+		}
+		#frame{
+			margin-right: auto;
+			margin-left: auto;
+			text-align:center;
+			max-width:800px;
+            padding-top: 15px;
+            padding-bottom: 15px;
+			padding-left:5%;
+            padding-right:5%;
+			box-shadow: 0 8px 6px -6px black;
+			background-color: wheat;
+		}
+		body{
+			background-color: #B6C5D0;
+			
+		}
+		img{
+			max-width:50%;
+			max-height:50%;
+		}
+
+		/*h1 {
+            background-color:black; 
+            color:white; 
+            text-align:center;}*/
+		#footer {
+            background-color:black; 
+            color:white; clear:both; 
+            text-align:center; 
+            padding:5px;
+		}
+		form{
+            /*color: white;*/
+			background-color:#484E53;
+            min-height: 60px;
+            font-family:cooper;
+            float:center;
+            border:2px solid green;
+            box-shadow: 0 8px 6px -6px black;
+		}
+        #logout{
+            right: 10px;
+        }
+        #input{
+            width: 80%;
+        }
+    </style>
+</head>
+<body>
+
+    <div id="frame">  
+        <h1>LEADERBOARD</h1>
+        <form>
+            <table>
+                <tr>
+                    <th>USER</th>
+                    <th>PUZZLE SOLVED</th>
+                </tr>
+                <tr>
+                    <td>ESHWAR</td>
+                    <td>3</td>
+                </tr>
+                <tr>
+                    <td>ESHWAR</td>
+                    <td>3</td>
+                </tr>
+                <tr>
+                    <td>ESHWAR</td>
+                    <td>3</td>
+                </tr>
+                <tr>
+                    <td>ESHWAR</td>
+                    <td>3</td>
+                </tr>
+                <tr>
+                    <td>ESHWAR</td>
+                    <td>3</td>
+                </tr>
+                <tr>
+                    <td>ESHWAR</td>
+                    <td>3</td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</body>
+</html>
