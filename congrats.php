43b5733a180b9bb28ed7dd29e726cdbe2a14a63e
eshwar <eshwarmsms@gmail.com>
2017-03-24 23:56:31 +0530

    added security

--- congrats.php
+++ congrats.php
@@ -5,11 +5,41 @@
     
 </head>
 <body>
+    <?php
+        session_start(); //starts the session
+        if($_SESSION['user']){ //checks if user is logged in
+        }
+        else{
+            header("location:index.php"); // redirects if user is not logged in
+        }
+        $user = $_SESSION['user']; //assigns user value
+            
+        mysql_connect("in-cdbr-azure-south-c.cloudapp.net", "bf142daa6c0a1f","97095cdc", "treasurehunt") or die(mysql_error()); //Connect to server
+        mysql_select_db("treasurehunt") or die("Cannot connect to database"); //Connect to database
 
+        $query = mysql_query("SELECT * from users"); //Query the users table if there are matching rows equal to $username
+        $exists = mysql_num_rows($query); //Checks if username exists
+        if($exists > 0) //IF there are no returning rows or no existing username
+        {
+            while($row = mysql_fetch_assoc($query)) //display all rows from query
+            {
+                $table_current = $row['current'];
+                $table_hint = $row['hint'];
+            }
+            if($table_current < 31){
+                header("location:hunt.php");
+            }
+        }
+        else
+        {
+            header("location:hunt.php");
+        }
+
+    ?>
     <div id="frame">  
         <h1>CONGRATULATIONS</h1>
         <form>
-            <img src="images/comgrats.jpeg" alt="congrats">
+            <img src="images/congrats.jpeg" alt="congrats">
         </form>
     </div>
 </body>
